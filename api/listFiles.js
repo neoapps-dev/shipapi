7680e81639fb7bc68a770ffcf7cec2facffb2103
Asd22 Entertainment <158327205+neoapps-dev@users.noreply.github.com>
2024-06-06 17:47:53 +0300

    Update listFiles.js

--- api/listFiles.js
+++ api/listFiles.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = async (req, res) => {
-    const directoryPath = path.join(process.cwd(), 'v1'); // Assuming 'v1' is the directory you want to list files from
+    const directoryPath = path.join(process.cwd(), 'v1');
     try {
         const files = await fs.promises.readdir(directoryPath);
-        const fileNames = files.map(fileName => path.parse(fileName).name);
+        const filteredFiles = files.filter(fileName => fileName !== 'index');
+        const fileNames = filteredFiles.map(fileName => path.parse(fileName).name);
         res.status(200).json(fileNames);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
