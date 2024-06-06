91a7c1e413faf4f32c0293e6786f40439b3eb02c
Asd22 Entertainment <158327205+neoapps-dev@users.noreply.github.com>
2024-06-06 17:52:26 +0300

    Update pkgs.js

--- api/pkgs.js
+++ api/pkgs.js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
     const directoryPath = path.join(process.cwd(), 'v1');
     try {
         const files = await fs.promises.readdir(directoryPath);
+        // Filter out "index" file
         const filteredFiles = files.filter(fileName => fileName !== 'index');
         const fileNames = filteredFiles.map(fileName => path.parse(fileName).name);
         res.status(200).json(fileNames);
